c1cdcc6a240869d174827fc4c96a367f9f745120
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-08-18 08:13:43 +0000

    new

---
+++ Node Learning/AsyncEX.js
@@ -0,0 +1,18 @@
+import {promises, readFile, writeFile} from 'fs';
+const fs = promises;
+
+async function processFile() {
+    try {
+        const data = await fs.readFile('input.txt', 'utf8');
+        const processed = data.toUpperCase();
+        await fs.writeFile("output.txt", processed);
+        console.log("File processed and saved to output.txt");
+    } catch (err) {
+        console.log("Error:", err);
+
+    }
+    
+}
+
+processFile();
+console.log("Processing File...............");
